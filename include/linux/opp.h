ed23b7abd9c126222ba50e1e9e53088773e623a6
Andrei F <luxneb@gmail.com>
2013-01-02 22:58:47 +0100

    opp: De-obscurify OPP structs and disclose find_device_opp
    Move definitions of OPP structs in the header file so that its
    members are public and the struct is no longer obscure.
    
    Define interface for find_device_opp in the header file.
    
    Conflicts:
    
    	include/linux/opp.h

--- include/linux/opp.h
+++ include/linux/opp.h
@@ -18,7 +18,25 @@
 #include <linux/cpufreq.h>
 #include <linux/notifier.h>
 
-struct opp;
+struct opp {
+	struct list_head node;
+
+	bool available;
+	unsigned long rate;
+	unsigned long u_volt;
+
+	struct device_opp *dev_opp;
+	struct rcu_head head;
+};
+
+struct device_opp {
+	struct list_head node;
+
+	struct device *dev;
+	struct srcu_notifier_head head;
+	struct list_head opp_list;
+};
+
 struct device;
 
 enum opp_event {
@@ -26,6 +44,8 @@ enum opp_event {
 };
 
 #if defined(CONFIG_PM_OPP)
+
+struct device_opp *find_device_opp(struct device *dev);
 
 unsigned long opp_get_voltage(struct opp *opp);
 
