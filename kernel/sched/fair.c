a161bc2aaddb6182ff2f61123bce839c832b6b51
Chris Redpath <chris.redpath@arm.com>
2013-10-11 11:44:58 +0100

    sched: HMP: Change default HMP thresholds
    When the up-threshold is at 512 on TC2, behaviour looks OK since
    the graphic-related tasks are very heavy due to lack of a GPU.
    Increasing the up-threshold does not reduce power consumption.
    
    When a GPU is present, graphic tasks are much less CPU-heavy and
    so additional power may be saved by having a higher threshold.
    
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Liviu Dudau <Liviu.Dudau@arm.com>
    Signed-off-by: Jon Medhurst <tixy@linaro.org>
    
    Conflicts:
    
    	kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1587,7 +1587,7 @@ static inline void __update_group_entity_contrib(struct sched_entity *se) {}
  */
 
 unsigned int hmp_up_threshold = 700;
-unsigned int hmp_down_threshold = 256;
+unsigned int hmp_down_threshold = 512;
 
 unsigned int hmp_semiboost_up_threshold = 400;
 unsigned int hmp_semiboost_down_threshold = 150;
