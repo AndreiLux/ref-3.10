af0c051e765c52402a1965203d20246b7ff23ca7
siddardha naraharisetti <siddardhan@nvidia.com>
2014-04-07 19:21:58 -0700

    power: bq2419x: add different time for power on after charge complete shutdown
    If high charging source is connected and system goes to shutdown after
    full charging of battery then system can be stay on power off for longer
    time. Add different auto recharge time for power off state.
    
    Bug 1489717
    
    Change-Id: Iaf1c8721eef70b7c51c2b5bf0ba3c1a082995456
    Signed-off-by: siddardha naraharisetti <siddardhan@nvidia.com>
    Reviewed-on: http://git-master/r/393183
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Change-Id: I9a7d42737dfc748cbc79efc1abc5bca03e3e8a80
    Reviewed-on: http://git-master/r/404464
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>

--- drivers/power/bq2419x-charger.c
+++ drivers/power/bq2419x-charger.c
@@ -94,6 +94,7 @@ struct bq2419x_chip {
 	int				gpio_otg_iusb;
 	int				wdt_refresh_timeout;
 	int				wdt_time_sec;
+	int				auto_recharge_time_power_off;
 	bool				emulate_input_disconnected;
 
 	struct mutex			mutex;
@@ -1744,6 +1745,7 @@ static struct bq2419x_platform_data *bq2419x_dt_parse(struct i2c_client *client)
 		int wdt_timeout;
 		int chg_restart_time;
 		int suspend_polling_time;
+		int auto_recharge_time_power_off;
 		int temp_polling_time;
 		int thermal_temp;
 		unsigned int thermal_volt;
@@ -1827,6 +1829,13 @@ static struct bq2419x_platform_data *bq2419x_dt_parse(struct i2c_client *client)
 				"ti,watchdog-timeout", &wdt_timeout);
 		if (!ret)
 			pdata->bcharger_pdata->wdt_timeout = wdt_timeout;
+
+		ret = of_property_read_u32(batt_reg_node,
+			"ti,auto-recharge-time-power-off",
+			&auto_recharge_time_power_off);
+		if (!ret)
+			pdata->bcharger_pdata->auto_recharge_time_power_off =
+					auto_recharge_time_power_off;
 
 		ret = of_property_read_u32(batt_reg_node,
 				"ti,auto-recharge-time", &chg_restart_time);
@@ -2270,6 +2279,8 @@ static int bq2419x_probe(struct i2c_client *client,
 		goto skip_bcharger_init;
 	}
 
+	bq2419x->auto_recharge_time_power_off =
+			pdata->bcharger_pdata->auto_recharge_time_power_off;
 	bq2419x->wdt_time_sec = pdata->bcharger_pdata->wdt_timeout;
 	bq2419x->chg_restart_time = pdata->bcharger_pdata->chg_restart_time;
 	bq2419x->battery_presense = true;
@@ -2397,6 +2408,10 @@ static void bq2419x_shutdown(struct i2c_client *client)
 
 	if (bq2419x->cable_connected && bq2419x->in_current_limit > 500 &&
 			bq2419x->wdt_refresh_timeout) {
+		if ((bq2419x->chg_status == BATTERY_CHARGING_DONE)
+			&& bq2419x->auto_recharge_time_power_off)
+			bq2419x->wdt_refresh_timeout =
+				bq2419x->auto_recharge_time_power_off;
 		ret = battery_charging_system_reset_after(bq2419x->bc_dev,
 				bq2419x->wdt_refresh_timeout);
 		if (ret < 0)
