d1b427b08d073b2ff6252e7dcec9c2fcf2e62dd9
Daniel Rosenberg <drosen@google.com>
2014-05-12 16:54:10 -0700

    ARM64: tegra: Set up custom combo for reset
    Used keycombo driver to ignore a long hold on power for reset,
    and to reset based on Power+Volume down instead.
    
    Change-Id: Iaf5e62839ca94bf1ff920c2a0a602cb2a6e92919
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- arch/arm/mach-tegra/board-flounder-kbc.c
+++ arch/arm/mach-tegra/board-flounder-kbc.c
@@ -25,6 +25,11 @@
 #include <linux/io.h>
 #include <linux/gpio.h>
 #include <linux/gpio_keys.h>
+#include <linux/delay.h>
+#include <linux/keycombo.h>
+#include <linux/keyreset.h>
+#include <linux/reboot.h>
+#include <linux/workqueue.h>
 
 #include "iomap.h"
 #include "wakeups-t12x.h"
@@ -79,7 +84,81 @@ static struct platform_device flounder_int_keys_device = {
 	},
 };
 
+static struct keyreset_platform_data flounder_reset_keys_pdata = {
+	.key_down_delay = 10000,
+	.keys_down = {
+		KEY_POWER,
+		KEY_VOLUMEDOWN,
+		0
+	}
+};
+
+static struct platform_device flounder_reset_keys_device = {
+	.name = KEYRESET_NAME,
+	.dev.platform_data = &flounder_reset_keys_pdata,
+	.id = PLATFORM_DEVID_AUTO,
+};
+
+#define PWR_MISTOUCH TEGRA_GPIO_PI3
+
+static void clear_hw_reset(void)
+{
+	int ret = 0;
+	pr_info("power key held down\n");
+	ret = gpio_direction_output(PWR_MISTOUCH, 0);
+	if (ret < 0)
+		pr_err("[KEY] gpio_direction_output GPIO %d failed\n",
+								PWR_MISTOUCH);
+	msleep(100);
+	ret = gpio_direction_output(PWR_MISTOUCH, 1);
+	if (ret < 0)
+		pr_err("[KEY] gpio_direction_output GPIO %d failed\n",
+								PWR_MISTOUCH);
+}
+
+static void keep_hw_reset_clear(struct work_struct *);
+DECLARE_DELAYED_WORK(clear_restart_work, &keep_hw_reset_clear);
+
+static void keep_hw_reset_clear(struct work_struct *dummy)
+{
+	clear_hw_reset();
+	schedule_delayed_work(&clear_restart_work, msecs_to_jiffies(2000));
+}
+
+static void start_reset_clear(void *dummy)
+{
+	schedule_delayed_work(&clear_restart_work, msecs_to_jiffies(2000));
+}
+
+static void stop_clearing(void *dummy)
+{
+	cancel_delayed_work_sync(&clear_restart_work);
+}
+
+static struct keycombo_platform_data flounder_clear_hw_reset_pdata = {
+	.key_down_fn = &start_reset_clear,
+	.key_up_fn = &stop_clearing,
+	.keys_down = {
+		KEY_POWER,
+		0
+	}
+};
+
+static struct platform_device flounder_clear_hw_reset_device = {
+	.name = KEYCOMBO_NAME,
+	.dev.platform_data = &flounder_clear_hw_reset_pdata,
+	.id = PLATFORM_DEVID_AUTO,
+};
+
 int __init flounder_kbc_init(void)
 {
+	if (platform_device_register(&flounder_reset_keys_device))
+		pr_warn(KERN_WARNING "%s: register key reset failure\n",
+								__func__);
+
+	gpio_request(PWR_MISTOUCH, "PWR_MISTOUCH");
+	if (platform_device_register(&flounder_clear_hw_reset_device))
+		pr_warn(KERN_WARNING "%s: register clear hw reset failure\n",
+								__func__);
 	return platform_device_register(&flounder_int_keys_device);
 }
