6cc691424222845a8d17670d9c6b7c8b69e513b7
jim_sun <jim_sun@htc.com>
2014-05-19 20:53:34 +0800

    misc:qcom-mdm-9k: Correct getting enable modem ramdump flag.
    This patch
    -Correct getting enable modem ramdump flag from subsystem_restart
    module.
    
    Change-Id: Icf94abb38678765cffd00d3e502c5407b5ab7f62
    Signed-off-by: jim_sun <jim_sun@htc.com>

--- drivers/misc/qcom-mdm-9k/qcom_usb_modem_power.c
+++ drivers/misc/qcom-mdm-9k/qcom_usb_modem_power.c
@@ -87,6 +87,15 @@
 #define NV_WRITE_DONE		_IO(CHARM_CODE, 100)
 #define POWER_OFF_CHARM _IOW(CHARM_CODE, 101, int)
 
+#ifdef CONFIG_MSM_SUBSYSTEM_RESTART
+extern int get_enable_ramdumps(void);
+#else
+static int get_enable_ramdumps(void)
+{
+	return 0;
+}
+#endif
+
 static void mdm_enable_irqs(struct qcom_usb_modem *modem, bool is_wake_irq)
 {
 	if(is_wake_irq)
@@ -521,7 +530,7 @@ static void mdm_fatal(struct work_struct *ws)
 	}
 #endif
 
-	if (modem->mdm_debug_on) {
+	if (get_enable_ramdumps()) {
 		mdelay(3000);
 	}
 
@@ -1675,7 +1684,7 @@ static int mdm_subsys_shutdown(const struct subsys_data *crashed_subsys)
 	//Power down mdm
 	mdm_disable_irqs(modem, false);
 
-	if((modem->mdm_status & MDM_STATUS_RESETTING) && modem->mdm_debug_on)
+	if((modem->mdm_status & MDM_STATUS_RESETTING) && get_enable_ramdumps())
 	{
 		pr_info("%s: Need to capture MDM RAM Dump, don't Pulling RESET gpio LOW here to prevent MDM memory data loss\n", __func__);
 	}
@@ -1755,7 +1764,7 @@ static int mdm_subsys_ramdumps(int want_dumps, const struct subsys_data *crashed
 
 		mdm_disable_irqs(modem, false);
 
-		if((modem->mdm_status & MDM_STATUS_RESETTING) && modem->mdm_debug_on)
+		if((modem->mdm_status & MDM_STATUS_RESETTING) && get_enable_ramdumps())
 		{
 			pr_info("%s: Need to capture MDM RAM Dump, don't Pulling RESET gpio LOW here to prevent MDM memory data loss\n", __func__);
 		}
