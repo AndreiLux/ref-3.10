84cb69b26d11192a62ba4975593bc11bc778ef70
Krishna Sitaraman <ksitaraman@nvidia.com>
2014-05-30 13:31:44 -0700

    ARM: T132: DVFS: Increase cpu frequency granularity below vmin
    Granularity set at 25.5mhz upto 1020Mhz.
    
    Bug 1509711
    
    Change-Id: I80d4e78a2c8d1fe995a88ed220b7831b500e162f
    Signed-off-by: Krishna Sitaraman <ksitaraman@nvidia.com>
    Reviewed-on: http://git-master/r/417245
    Reviewed-by: Mandar Padmawar <mpadmawar@nvidia.com>
    Tested-by: Mandar Padmawar <mpadmawar@nvidia.com>

--- arch/arm/mach-tegra/tegra12_clocks.c
+++ arch/arm/mach-tegra/tegra12_clocks.c
@@ -9259,7 +9259,7 @@ struct tegra_cpufreq_table_data *tegra_cpufreq_table_get(void)
 #else
 
 #define GRANULARITY_KHZ   25500
-#define GRANULARITY_END   204000
+#define GRANULARITY_END 1020000
 #define CPU_THROTTLE_FREQ 408000
 #define CPU_SUSPEND_FREQ  408000
 
