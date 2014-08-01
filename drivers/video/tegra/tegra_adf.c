9fa74918f508958b99d0314581683969454f9eb3
Krishna Reddy <vdumpa@nvidia.com>
2014-08-01 14:33:42 -0700

    video: tegra: adf: replace nvmap_alloc_dmabuf with v4l2 API
    Replace deprecated nvmap_alloc_dmabuf with V4L2 videobuf2 contig dma API.
    
    Bug 1540346
    
    Change-Id: I06dd4a64a5a548d29443a0dfb8e4ccdeee62644c
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/450681
    Reviewed-by: Mandar Padmawar <mpadmawar@nvidia.com>
    Tested-by: Mandar Padmawar <mpadmawar@nvidia.com>

--- drivers/video/tegra/tegra_adf.c
+++ drivers/video/tegra/tegra_adf.c
@@ -16,7 +16,8 @@
  */
 
 #include <linux/memblock.h>
-#include <linux/nvmap.h>
+#include <linux/gfp.h>
+#include <media/videobuf2-dma-contig.h>
 #include <video/adf.h>
 #include <video/adf_client.h>
 #include <video/adf_fbdev.h>
@@ -25,7 +26,6 @@
 
 #include "dc/dc_config.h"
 #include "dc/dc_priv.h"
-#include "nvmap/nvmap_priv.h"
 #include "tegra_adf.h"
 
 struct tegra_adf_info {
@@ -37,6 +37,7 @@ struct tegra_adf_info {
 #endif
 	struct tegra_dc			*dc;
 	struct tegra_fb_data		*fb_data;
+	void				*vb2_dma_conf;
 };
 
 struct tegra_adf_flip_data {
@@ -1020,32 +1021,14 @@ static int tegra_adf_intf_blank(struct adf_interface *intf, u8 state)
 	return 0;
 }
 
-static int tegra_adf_zero_dma_buf(struct dma_buf *buf)
-{
-	unsigned long i;
-	int err;
-
-	err = dma_buf_begin_cpu_access(buf, 0, buf->size, DMA_TO_DEVICE);
-	if (err < 0)
-		return err;
-
-	for (i = 0; i < buf->size / PAGE_SIZE; i++) {
-		void *mapped = dma_buf_kmap(buf, i);
-		memset(mapped, 0, PAGE_SIZE);
-		dma_buf_kunmap(buf, i, mapped);
-	}
-
-	dma_buf_end_cpu_access(buf, 0, buf->size, DMA_TO_DEVICE);
-
-	return 0;
-}
-
 static int tegra_adf_intf_alloc_simple_buffer(struct adf_interface *intf,
 		u16 w, u16 h, u32 format,
 		struct dma_buf **dma_buf, u32 *offset, u32 *pitch)
 {
 	size_t i;
-	int err;
+	struct tegra_adf_info *adf_info = adf_intf_to_tegra(intf);
+	const struct vb2_mem_ops *mem_ops = &vb2_dma_contig_memops;
+	void *vb2_buf;
 	bool format_valid = false;
 
 	for (i = 0; i < ARRAY_SIZE(tegra_adf_formats); i++) {
@@ -1060,16 +1043,18 @@ static int tegra_adf_intf_alloc_simple_buffer(struct adf_interface *intf,
 
 	*offset = 0;
 	*pitch = ALIGN(w * adf_format_bpp(format) / 8, 64);
-	*dma_buf = nvmap_alloc_dmabuf(PAGE_ALIGN(h * *pitch), 0,
-			NVMAP_HANDLE_WRITE_COMBINE, 0);
-	if (IS_ERR(*dma_buf))
-		return PTR_ERR(*dma_buf);
 
-	err = tegra_adf_zero_dma_buf(*dma_buf);
-	if (err < 0)
-		dma_buf_put(*dma_buf);
+	vb2_buf = mem_ops->alloc(adf_info->vb2_dma_conf,
+				 h * *pitch, __GFP_HIGHMEM);
+	if (IS_ERR(vb2_buf))
+		return PTR_ERR(vb2_buf);
 
-	return err;
+	*dma_buf = mem_ops->get_dmabuf(vb2_buf);
+	mem_ops->put(vb2_buf);
+	if (!*dma_buf)
+		return -ENOMEM;
+
+	return 0;
 }
 
 static int tegra_adf_intf_describe_simple_post(struct adf_interface *intf,
@@ -1266,6 +1251,10 @@ struct tegra_adf_info *tegra_adf_init(struct platform_device *ndev,
 	err = adf_attachment_allow(&adf_info->base, &adf_info->eng,
 			&adf_info->intf);
 	if (err < 0)
+		goto err_attach;
+
+	adf_info->vb2_dma_conf = vb2_dma_contig_init_ctx(&ndev->dev);
+	if ((err = IS_ERR(adf_info->vb2_dma_conf)))
 		goto err_attach;
 
 	if (dc->out->n_modes) {
