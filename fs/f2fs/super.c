1d4e11b890ba46d5a8cc8b398869dae9bccede94
Jaegeuk Kim <jaegeuk@kernel.org>
2014-08-08 15:37:41 -0700

    f2fs: give a chance to mount again when encountering errors
    This patch gives another chance to try mount process when we encounter an error.
    This makes an effect on the roll-forward recovery failures as well.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    
    Conflicts:
    	fs/f2fs/super.c
    
    Change-Id: I85fa80945ce678484c4c74e1ad2eba9169029739

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -989,9 +989,12 @@ static int f2fs_fill_super(struct super_block *sb, void *data, int silent)
 	struct inode *root;
 	long err = -EINVAL;
 	const char *descr = "";
+	bool retry = true;
 	int i;
 
 	f2fs_msg(sb, KERN_INFO, "mounting..");
+
+try_onemore:
 	/* allocate memory for f2fs-specific super block info */
 	sbi = kzalloc(sizeof(struct f2fs_sb_info), GFP_KERNEL);
 	if (!sbi)
@@ -1185,9 +1188,10 @@ get_cp:
 				goto get_cp;
 			}
 			f2fs_msg(sb, KERN_ERR,
-				"cannot recover all fsync data errno=%ld", err);
+				"Cannot recover all fsync data errno=%ld", err);
 			/* checkpoint what we have */
 			write_checkpoint(sbi, false);
+			goto free_kobj;
 		}
 	}
 
@@ -1229,6 +1233,12 @@ free_sb_buf:
 	brelse(raw_super_buf);
 free_sbi:
 	kfree(sbi);
+	/* give only one another chance */
+	if (retry) {
+		retry = !retry;
+		shrink_dcache_sb(sb);
+		goto try_onemore;
+	}
 	f2fs_msg(sb, KERN_ERR, "mount failed");
 	return err;
 }
