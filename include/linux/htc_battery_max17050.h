723d0ab51972bfc376d943376a206eb5604dcc59
keny_wu <keny_wu@htc.com>
2014-08-25 17:25:34 +0800

    power: max17050: add low temperature parameter change by battery id
    Let SOC calculation more smoothly if battery temperature low
    
    Change-Id: Iac224bf4a66aa675f1f5bf38f3d99cfad193a88e
    Signed-off-by: keny_wu <keny_wu@htc.com>

--- include/linux/htc_battery_max17050.h
+++ include/linux/htc_battery_max17050.h
@@ -16,6 +16,18 @@
 
 #include<linux/types.h>
 
+#define FLOUNDER_BATTERY_ID_RANGE_SIZE	(2)
+#define FLOUNDER_BATTERY_PARAMS_SIZE	(3)
+
+struct flounder_battery_adjust_by_id {
+	int id;
+	unsigned int id_range[FLOUNDER_BATTERY_ID_RANGE_SIZE];
+	int temp_normal2low_thr;
+	int temp_low2normal_thr;
+	unsigned int temp_normal_params[FLOUNDER_BATTERY_PARAMS_SIZE];
+	unsigned int temp_low_params[FLOUNDER_BATTERY_PARAMS_SIZE];
+};
+
 struct htc_battery_max17050_ops {
 	int (*get_vcell)(int *batt_volt);
 	int (*get_battery_current)(int *batt_curr);
@@ -25,6 +37,14 @@ struct htc_battery_max17050_ops {
 	int (*get_ocv)(int *batt_ocv);
 	int (*get_battery_charge)(int *batt_charge);
 	int (*get_battery_charge_ext)(int64_t *batt_charge_ext);
+};
+
+#define FLOUNDER_BATTERY_ID_MAX	(10)
+struct flounder_battery_platform_data {
+	const char *batt_id_channel_name;
+	struct flounder_battery_adjust_by_id
+		batt_params[FLOUNDER_BATTERY_ID_MAX];
+	int batt_params_num;
 };
 
 int htc_battery_max17050_gauge_register(struct htc_battery_max17050_ops *ops);
