29a6ff527a292bc3ee2a54f1f0490a7b403702e5
Sasha Levitskiy <sanek@google.com>
2014-09-03 19:45:37 -0700

    Flounder: Camera: export factory calibration to /sys/firmware
    Change-Id: Iab0930956c29e4a660b32a5ff94427b6ab99a30a
    Signed-off-by: Sasha Levitskiy <sanek@google.com>

--- arch/arm64/mach-tegra/htc_awb_cal.c
+++ arch/arm64/mach-tegra/htc_awb_cal.c
@@ -1,6 +1,6 @@
-/* arch/arm/mach-msm/htc_awb_cal.c */
-/* Code to extract Camera AWB calibration information from ATAG
-set up by the bootloader.
+/* arch/arm/mach-msm/htc_awb_cal.c
+ * Code to extract Camera AWB calibration information from ATAG
+ * set up by the bootloader.
 */
 
 #include <linux/module.h>
@@ -10,80 +10,104 @@ set up by the bootloader.
 #include <linux/proc_fs.h>
 #include <asm/setup.h>
 
-/* for outputing file to filesystem : /data/awb_calibration_data_hboot.txt */
-#include <linux/fs.h>
 #include <linux/syscalls.h>
-
 #include <linux/of.h>
 
-/* HTC_START, use dt for calibration data */
-#define CALIBRATION_DATA_PATH "/calibration_data"
-#define CAM_AWB_CAL_DATA "cam_awb"
-#define AWB_CAL_SIZE	889  //size of calibration data
+const char awb_cam_cal_data_path[] = "/calibration_data";
+const char awb_cam_cal_data_tag[] = "cam_awb";
 
-static ssize_t awb_calibration_show(struct device *dev,
-        struct device_attribute *attr, char *buf, size_t index)
+const loff_t awb_cal_front_offset = 0x1000;	/* Offset of the front cam data */
+#define AWB_CAL_SIZE 897	/* size of the calibration data */
+
+static ssize_t awb_calibration_get(char *buf, size_t count, loff_t off,
+				   bool is_front)
 {
-    ssize_t ret = AWB_CAL_SIZE;
-    int p_size = 0;
-    unsigned char *p_data = NULL;
-    struct device_node *offset = of_find_node_by_path(CALIBRATION_DATA_PATH);
+	int p_size = 0;
+	unsigned char *p_data = NULL;
+	struct device_node *dev_node = NULL;
 
-    if (offset) {
-        /* of_get_property will return address of property, */
-        /* and fill the length to *p_size */
-        p_data = (unsigned char*) of_get_property(offset,
-                                                    CAM_AWB_CAL_DATA, &p_size);
-    }
+	dev_node = of_find_node_by_path(awb_cam_cal_data_path);
+	if (dev_node)
+		/* of_get_property will return address of property,
+		 * and fill the length to *p_size */
+		p_data = (unsigned char *)of_get_property(dev_node,
+							  awb_cam_cal_data_tag,
+							  &p_size);
 
-	memcpy(buf, p_data + index, ret);
+	if (p_data == NULL) {
+		pr_err("%s: Unable to get calibration data", __func__);
+		return 0;
+	}
 
-	return ret;
+	if (off >= AWB_CAL_SIZE)
+		return 0;
+
+	if (off + count > AWB_CAL_SIZE)
+		count = AWB_CAL_SIZE - off;
+
+	p_data += is_front ? awb_cal_front_offset : 0;
+	memcpy(buf, p_data + off, count);
+
+	return count;
 }
 
-static ssize_t awb_calibration_back_show(struct device *dev,
-        struct device_attribute *attr, char *buf)
+static ssize_t awb_calibration_back_read(struct file *filp,
+					 struct kobject *kobj,
+					 struct bin_attribute *attr,
+					 char *buf, loff_t off, size_t count)
 {
-    return awb_calibration_show(dev, attr, buf, 0);
+	return awb_calibration_get(buf, count, off, false);
 }
 
-static ssize_t awb_calibration_front_show(struct device *dev,
-        struct device_attribute *attr, char *buf)
+static ssize_t awb_calibration_front_read(struct file *filp,
+					  struct kobject *kobj,
+					  struct bin_attribute *attr,
+					  char *buf, loff_t off, size_t count)
 {
-    return awb_calibration_show(dev, attr, buf, 0x1000);
+	return awb_calibration_get(buf, count, off, true);
 }
 
-static DEVICE_ATTR(factory, 0444, awb_calibration_back_show, NULL);
-static DEVICE_ATTR(factory_front, 0444, awb_calibration_front_show, NULL);
+static struct bin_attribute factory_back = {
+	.attr.name = "factory_back",
+	.attr.mode = 0444,
+	.size = AWB_CAL_SIZE,
+	.read = awb_calibration_back_read,
+};
 
-static struct kobject *cam_awb_cal;
+static struct bin_attribute factory_front = {
+	.attr.name = "factory_front",
+	.attr.mode = 0444,
+	.size = AWB_CAL_SIZE,
+	.read = awb_calibration_front_read,
+};
 
 static int cam_get_awb_cal(void)
 {
-	int ret ;
+	int ret;
+	struct kobject *cam_awb_cal;
 
-	/* Create /sys/android_camera_awb_cal/awb_cal */
-	cam_awb_cal = kobject_create_and_add("android_camera_awb_cal", NULL);
+	cam_awb_cal = kobject_create_and_add("camera_awb_cal", firmware_kobj);
 	if (cam_awb_cal == NULL) {
-		pr_info("[CAM]%s: subsystem_register failed\n", __func__);
+		pr_err("%s: failed to create /sys/camera_awb_cal\n",
+		       __func__);
 		return -ENOMEM;
 	}
 
-	ret = sysfs_create_file(cam_awb_cal, &dev_attr_factory.attr);
+	ret = sysfs_create_bin_file(cam_awb_cal, &factory_back);
 	if (ret) {
-		pr_info("[CAM]cam_get_awb_cal:: sysfs_create_file failed\n");
+		pr_err("%s: failed to create back camera file\n", __func__);
 		kobject_del(cam_awb_cal);
 		return ret;
 	}
 
-	ret = sysfs_create_file(cam_awb_cal, &dev_attr_factory_front.attr);
+	ret = sysfs_create_bin_file(cam_awb_cal, &factory_front);
 	if (ret) {
-		pr_info("[CAM]cam_get_awb_cal_front:: sysfs_create_file failed\n");
+		pr_err("%s: failed to create front camera file\n", __func__);
 		kobject_del(cam_awb_cal);
 		return ret;
 	}
 
-	return 0 ;
+	return 0;
 }
 
 late_initcall(cam_get_awb_cal);
