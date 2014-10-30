a36b57f456d99290fa1b83fe933243fb016fdc06
Marvin Tsai <marvin_tsai@htc.com>
2014-10-30 17:21:24 +0800

    tegra: video: host: use correct index to suspend a channel
    Global channel index should be used instead of device's own index.
    
    Change-Id: I654b647472438264e5af25163aebb232bf81fad4

--- drivers/video/tegra/host/nvhost_acm.c
+++ drivers/video/tegra/host/nvhost_acm.c
@@ -774,7 +774,7 @@ int nvhost_module_disable_clk(struct device *dev)
 
 	for (index = 0; index < pdata->num_channels; index++)
 		if (pdata->channels[index] &&
-			test_bit(index, &host->allocated_channels))
+			test_bit(pdata->channels[index]->chid, &host->allocated_channels))
 			nvhost_channel_suspend(pdata->channels[index]);
 
 	for (index = 0; index < pdata->num_clks; index++)
