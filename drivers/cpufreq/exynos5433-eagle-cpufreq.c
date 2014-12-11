88691d3c4c0f12bed3b577e9aa413c5862090ce8
Andrei F <luxneb@gmail.com>
2014-12-11 20:23:15 +0400

    cpufreq: exynos: Allow Exynos5433 big cores to 2GHz

--- drivers/cpufreq/exynos5433-eagle-cpufreq.c
+++ drivers/cpufreq/exynos5433-eagle-cpufreq.c
@@ -404,7 +404,7 @@ static void __init set_volt_table_CA15(void)
 		max_support_idx_CA15 = L7;	/* 1.8 GHz */
 		break;
 	default :
-		max_support_idx_CA15 = L6;	/* 1.9 GHz */
+		max_support_idx_CA15 = L5;	/* 2.0 GHz */
 	}
 
 	if (is_max_limit_sample() == 1)
